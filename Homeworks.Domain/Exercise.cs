7202e16aeca7c18f8b8f83708b163a1936362205
Santiago Mendez <santi17mendez@hotmail.com>
2018-08-30 19:00:50 -0300

    Base Projects and domain

---
+++ Homeworks.Domain/Exercise.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Homeworks.Domain
+{
+    public class Exercise
+    {
+        public Guid Id {get; set;}
+        public string Problem {get; set;}
+        public int Score {get; set;}
+    }
+}
