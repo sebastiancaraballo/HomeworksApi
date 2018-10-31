d42ce4f4c22662583775ffb79d2c58622bd3458e
Santiago Mendez <santi17mendez@hotmail.com>
2018-10-31 02:59:47 -0300

    Clase 10

--- Codigo/Angular/src/app/app.module.ts
+++ Codigo/Angular/src/app/app.module.ts
@@ -1,23 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeworksListComponent } from './homeworks-list/homeworks-list.component';
 import { HomeworksFilterPipe } from './homeworks-list/homeworks-filter.pipe';
 import { HomeworksService } from './services/homeworks.service';
 import { StarComponent } from './star/star.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeworksListComponent,
     HomeworksFilterPipe,
-    StarComponent
+    StarComponent,
+    WelcomeComponent
   ],
   imports: [
     FormsModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+        { path: 'homeworks', component: HomeworksListComponent },
+        { path: 'homeworks/:id', component: HomeworksListComponent }, // esto sería otro componente! por ejemplo uno que muestre el detalle de las tareas
+        { path: 'welcome', component:  WelcomeComponent }, 
+        { path: '', redirectTo: 'welcome', pathMatch: 'full' }, // configuramos la URL por defecto
+        { path: '**', redirectTo: 'welcome', pathMatch: 'full'} //cualquier otra ruta que no matchee, va a ir al WelcomeComponent, aca podría ir una pagina de error tipo 404 Not Found
+    ])
   ],
   providers: [
     HomeworksService
